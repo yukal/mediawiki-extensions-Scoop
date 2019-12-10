8761b10d16fc405e2d7e2333c872f36cc1595a4f
Alexander Yukal <yukal@email.ua>
2019-12-10 13:29:11 +0200

    add localization

---
+++ WikiScoop.alias.i18n.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Aliases for Special:WikiScoop
+ * https://www.mediawiki.org/wiki/Manual:Special_pages#The_aliases_file
+ *
+ * @file
+ * @ingroup Extensions
+ * @version 1.0
+ * @author Alexander Yukal <yukal@email.ua>
+ */
+
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+    'WikiScoop' => [ 'Wiki-Scoop', 'Wiki Scoop' ],
+];
+
+/** Russian (русский) */
+$specialPageAliases['ru'] = [
+    'WikiScoop' => [ 'Wiki-Scoop', 'Wiki ковш' ],
+];
+
+/** Ukrainian (українська) */
+$specialPageAliases['uk'] = [
+    'WikiScoop' => [ 'Wiki-Scoop', 'Wiki ківш' ],
+];
