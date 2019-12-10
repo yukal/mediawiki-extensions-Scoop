d9cb62bc1ad8c19dc2f369f104077e8905a83914
Alexander Yukal <yukal@email.ua>
2019-12-10 13:29:33 +0200

    add static

---
+++ static/ext.wikiScoop.js
@@ -0,0 +1,23 @@
+/**
+ * WikiScoop JS Module
+ *
+ * MediaWiki Resource Module
+ * Description.
+ *
+ * MediaWiki Manual
+ * https://doc.wikimedia.org/mediawiki-core/master/js/#!/api/mw
+ * https://doc.wikimedia.org/mediawiki-core/master/js/#!/api/jQuery
+ * https://www.mediawiki.org/wiki/Manual:$wgResourceModules
+ * https://www.mediawiki.org/wiki/Adding_javascript_to_wiki_pages
+ * https://www.mediawiki.org/wiki/ResourceLoader/Core_modules
+ *
+ * @file
+ * @ingroup Extensions
+ * @version 1.0
+ * @author Alexander Yukal <yukal@email.ua>
+ * @license https://opensource.org/licenses/MIT MIT License
+ */
+(function ($, mw) {
+    'use strict';
+    // ...
+}(jQuery, mediaWiki));
